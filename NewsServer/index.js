3f82a1753284844f72a1c6af21fe7d4933406adf
Yahor Laptsenok <yahor_laptsenok@epam.com>
2019-01-18 16:15:26 +0300

    return static set of news

--- NewsServer/index.js
+++ NewsServer/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const fs = require("fs");
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!' + fs.readFileSync('cnbc-articles.json')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
