3560e3d92322869ce48fa825b8e4143672d2e4ff
Yahor Laptsenok <yahor_laptsenok@epam.com>
2019-01-20 14:52:05 +0300

    handle post requests

--- NewsServer/index.js
+++ NewsServer/index.js
@@ -7,12 +7,26 @@ app.get('/', (req, res) => {
   id = req.param('id');
   console.log(id);
   if (id) {
-    res.send(JSON.parse(fs.readFileSync('cnbc-articles.json'))[id]);
+    res.send(readFile()[id]);
   } else {
-    res.send(JSON.parse(fs.readFileSync('cnbc-articles.json')));
+    res.send(readFile());
   }
 });
 
-app.get('/');
+readFile = () => {
+  return JSON.parse(fs.readFileSync('cnbc-articles.json'));
+}
+
+writeFile = data => {
+  fs.writeFileSync('cnbc-articles.json', data);
+}
+
+app.post('/', (req, res) => {
+  var newEntry = req.param('entry');
+  var data = readFile();
+  data.push(newEntry);
+  writeFile(JSON.stringify(data));
+  res.send('entry uploaded');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
