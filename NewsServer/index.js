0d1dc40b9c1763cef097fbe66560508ab7c58b63
Yahor Laptsenok <yahor_laptsenok@epam.com>
2019-01-20 13:51:43 +0300

    add param to get

--- NewsServer/index.js
+++ NewsServer/index.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
-const fs = require("fs");
-const port = 3000
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!' + fs.readFileSync('cnbc-articles.json')))
+app.get('/', (req, res) => {
+  id = req.param('id');
+  console.log(id);
+  if (id) {
+    res.send(JSON.parse(fs.readFileSync('cnbc-articles.json'))[id]);
+  } else {
+    res.send(JSON.parse(fs.readFileSync('cnbc-articles.json')));
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/');
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
