b4564084ef9a5ba1f5cc76adc25b0452ab903e79
Yahor Laptsenok <yahor_laptsenok@epam.com>
2019-02-09 14:15:16 +0300

    get facebook login to work

--- NewsServer/index.js
+++ NewsServer/index.js
@@ -2,11 +2,17 @@ require('dotenv').load();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
+const https = require('https');
+const fs = require('fs');
 const Strategy = require('passport-facebook').Strategy;
-passport.use(new Strategy({
+const mongoose = require('mongoose');
+const app = express();
+const MongoStore = require('express-mongoose-store')(session, mongoose);
+passport.use('facebook', new Strategy({
     clientID: process.env['FACEBOOK_CLIENT_ID'],
     clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
-    callbackURL: '/auth/facebook/callback'
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email', 'first_name', 'middle_name', 'last_name']
   },
   function (accessToken, refreshToken, profile, done) {
     User.upsertFbUser(accessToken, refreshToken, profile, function (err, user) {
@@ -14,10 +20,26 @@ passport.use(new Strategy({
     });
   }
 ));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+app.use(session({
+  secret: 'secrettexthere',
+  saveUninitialized: true,
+  resave: true,
+  store: new MongoStore({
+    collection: 'sessions',
+    ttl: 60000
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
-const app = express();
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const port = 3000;
 const logger = createLogger({
@@ -27,8 +49,9 @@ const logger = createLogger({
     new transports.File({ filename: 'combined.log' })
   ]
 });
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/news');
+mongoose.connect('mongodb://127.0.0.1:27017/news', {
+  useNewUrlParser: true
+});
 
 const newsSchema = new mongoose.Schema({
   author: String,
@@ -40,23 +63,24 @@ const newsSchema = new mongoose.Schema({
 });
 const UserSchema = new mongoose.Schema({
   email: {
-        type: String, required: true,
-        trim: true, unique: true,
-        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   },
   facebookProvider: {
-        type: {
-              id: String,
-              token: String
-        },
-        select: false
+    type: {
+      id: String,
+      token: String
+    },
+    select: false
   }
 });
 const NewsModel = mongoose.model('newsmodel', newsSchema, 'newsentries');
 
 UserSchema.statics.upsertFbUser = function (accessToken, refreshToken, profile, cb) {
   const that = this;
-  console.log(accessToken);
   return this.findOne({
     'facebookProvider.id': profile.id
   }, function (err, user) {
@@ -88,10 +112,15 @@ app.use(bodyParser.json());
 
 isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated())
-      return next();
+    return next();
 
   res.sendStatus(401);
 }
+
+app.get('/#_=_', (req, res) => {
+  console.log('hash')
+  res.redirect('/');
+})
 
 app.get('/', isLoggedIn, (req, res) => {
   const id = req.query.id;
@@ -199,12 +228,12 @@ app.get("/auth/facebook/callback",
         
 }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(port, () => console.log(`News app listening on port ${port}!`));
 
-app.use(function(err, req, res) {
-  console.error(err);
-  res.status(500).send(err.message);
+app.use(function(req, res, next) {
+  res.status(500).send();
 });
-
-app.use(session);
-app.use(passport.initialize());
